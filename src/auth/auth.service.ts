503a6e5299e954c7b3b70025e5ba5d9ab5daa378
Bilal Rafiq <bilal.q@dplit.com>
2025-04-23 17:17:50 +0500

    Fixing refresh

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,8 +21,10 @@ export class AuthService {
 
   async login(user: User) {
     const payload = { email: user.email, sub: user._id };
+    const { password, ...userWithoutPassword } = user.toObject();
     return {
       access_token: this.jwtService.sign(payload),
+      user: userWithoutPassword
     };
   }
 
